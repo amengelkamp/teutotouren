7ea95cd3ba770e9e6f1ad4aea9c1ec5e6eb4cb96
amengelkamp <aliciamengelkamp@icloud.com>
2025-01-28 08:06:51 +0100

    visual fittings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 
 // import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -19,22 +19,19 @@ function App() {
             alt="Wandern im Wald 01" 
           />
           <div className="searchbarWithFilters">
-            <p>searchbarWithFilters</p>
-            <div className="locationService"></div>
-              <p>locationService</p>
-            <div className="formOfTravel"></div>
-              <p>formOfTravel</p>
-            <div className="travelLength"></div>
-              <p>formOfTravel</p>
-            <div className="vacationLength"></div>
-              <p>travelLength</p>
-            <div className="accommodation"></div>
-              <p>accommodation</p>
-            <button>Suchen</button>
+            <div className="locationService">Dein Standort</div>
+
+            <div className="formOfTravel">formOfTravel</div>
+            <div className="travelLength">travelLength</div>
+            <div className="vacationLength">vacationLength</div>
+            <div className="accommodation">accomodation</div>
+            <button className="searchButton">Suchen</button>
           </div>
       </div>
         </div>
   )
+
+  
 }
 
 export default App
