7bec2f9dbd4e4113cc1d84055dc6e933e72ffd39
alicia <hi@thisisalicia.de>
2026-06-28 18:34:24 +0200

    feat: Hermannsdenkmal-Foto in Teutoburger-Wald-Kachel + .gitignore für public/images

--- src/regionShowcase/regionShowcase.jsx
+++ src/regionShowcase/regionShowcase.jsx
@@ -3,12 +3,12 @@ import axios from 'axios';
 import './regionShowcase.css';
 
 const REGIONEN = [
-    { value: 'Teutoburger Wald', label: 'Teutoburger Wald', color: '#2d5a27' },
-    { value: 'Schwarzwald',      label: 'Schwarzwald',      color: '#1a3d2e' },
-    { value: 'Eifel',            label: 'Eifel',            color: '#2c4a6e' },
-    { value: 'Harz',             label: 'Harz',             color: '#4a4a6e' },
-    { value: 'Sauerland',        label: 'Sauerland',        color: '#2e5c4a' },
-    { value: 'Rheinsteig',       label: 'Rheinsteig',       color: '#1e4a5c' },
+    { value: 'Teutoburger Wald', label: 'Teutoburger Wald', color: '#2d5a27', image: '/images/region_teutoburger_wald.jpg' },
+    { value: 'Schwarzwald',      label: 'Schwarzwald',      color: '#1a3d2e', image: null },
+    { value: 'Eifel',            label: 'Eifel',            color: '#2c4a6e', image: null },
+    { value: 'Harz',             label: 'Harz',             color: '#4a4a6e', image: null },
+    { value: 'Sauerland',        label: 'Sauerland',        color: '#2e5c4a', image: null },
+    { value: 'Rheinsteig',       label: 'Rheinsteig',       color: '#1e4a5c', image: null },
 ];
 
 const RegionShowcase = ({ onRegionSelect }) => {
@@ -34,7 +34,10 @@ const RegionShowcase = ({ onRegionSelect }) => {
                             className={`regionCard${clickable ? '' : ' regionCardDisabled'}`}
                             onClick={() => clickable && onRegionSelect(r.value)}
                         >
-                            <div className="regionCardPlaceholder" style={{ background: r.color }} />
+                            {r.image
+                                ? <img className="regionCardPlaceholder" src={r.image} alt={r.label} style={{ objectFit: 'cover' }} />
+                                : <div className="regionCardPlaceholder" style={{ background: r.color }} />
+                            }
                             <div className="regionCardContent">
                                 <span className="regionCardLabel">{r.label}</span>
                                 <span className="regionCardSub">{sub}</span>
