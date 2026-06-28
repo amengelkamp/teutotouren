fc032c9e979444498549ddde36418d8917cd4c09
alicia <hi@thisisalicia.de>
2026-06-28 18:48:52 +0200

    feat: Harz-Foto in Regionskachel eingebunden

--- src/regionShowcase/regionShowcase.jsx
+++ src/regionShowcase/regionShowcase.jsx
@@ -6,7 +6,7 @@ const REGIONEN = [
     { value: 'Teutoburger Wald', label: 'Teutoburger Wald', color: '#2d5a27', image: '/images/region_teutoburger_wald.jpg' },
     { value: 'Schwarzwald',      label: 'Schwarzwald',      color: '#1a3d2e', image: '/images/region_schwarzwald.jpg' },
     { value: 'Eifel',            label: 'Eifel',            color: '#2c4a6e', image: '/images/region_eifel.jpg' },
-    { value: 'Harz',             label: 'Harz',             color: '#4a4a6e', image: null },
+    { value: 'Harz',             label: 'Harz',             color: '#4a4a6e', image: '/images/region_harz.jpg' },
     { value: 'Sauerland',        label: 'Sauerland',        color: '#2e5c4a', image: null },
     { value: 'Rheinsteig',       label: 'Rheinsteig',       color: '#1e4a5c', image: null },
 ];
