2dceae24593e48ac5f13b13accace523c27d7be3
alicia <hi@thisisalicia.de>
2026-06-28 18:53:37 +0200

    feat: Rheinsteig-Foto in Regionskachel eingebunden

--- src/regionShowcase/regionShowcase.jsx
+++ src/regionShowcase/regionShowcase.jsx
@@ -8,7 +8,7 @@ const REGIONEN = [
     { value: 'Eifel',            label: 'Eifel',            color: '#2c4a6e', image: '/images/region_eifel.jpg' },
     { value: 'Harz',             label: 'Harz',             color: '#4a4a6e', image: '/images/region_harz.jpg' },
     { value: 'Sauerland',        label: 'Sauerland',        color: '#2e5c4a', image: '/images/region_sauerland.jpg' },
-    { value: 'Rheinsteig',       label: 'Rheinsteig',       color: '#1e4a5c', image: null },
+    { value: 'Rheinsteig',       label: 'Rheinsteig',       color: '#1e4a5c', image: '/images/region_rheinsteig.jpg' },
 ];
 
 const RegionShowcase = ({ onRegionSelect }) => {
